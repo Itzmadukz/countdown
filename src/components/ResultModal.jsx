2776b633f7655b5d341f7b0f6cfedbe764db15ef
Christopher Maduka <61519473+Itzmadukz@users.noreply.github.com>
2024-04-11 19:52:15 -0400

    Update: Resultmodal DOM Position

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom"
 
 const ResultMoadal = forwardRef(function ResultModal({ targetTime, remainingTime, OnReset }, ref) {
     const dialogue = useRef()
@@ -17,16 +18,19 @@ const ResultMoadal = forwardRef(function ResultModal({ targetTime, remainingTime
         }
     })
 
-    return <dialog ref={dialogue} className="result-modal" onClose={OnReset}>
-        {userLost && <h2>You Lost</h2>}
-        {!userLost && <h2>Your score: {score}</h2>}
-        <p>The target time was <strong>{targetTime} seconds</strong> </p>
-        <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong> </p>
+    return createPortal(
+        <dialog ref={dialogue} className="result-modal" onClose={OnReset}>
+            {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2>Your score: {score}</h2>}
+            <p>The target time was <strong>{targetTime} seconds</strong> </p>
+            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong> </p>
 
-        <form method="dialog" onSubmit={OnReset}>
-            <button>Close</button>
-        </form>
-    </dialog>
+            <form method="dialog" onSubmit={OnReset}>
+                <button>Close</button>
+            </form>
+        </dialog>,
+        document.getElementById('modal')
+    )
 })
 
 export default ResultMoadal
